7d02c70ca69f06d5a880280919a76dcda3dbf6b9
Ashfakul Islam <r4pido3@gmail.com>
2023-10-18 00:30:24 +0600

    product adding function added and its insert into db

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // database connection
+    const techBuddyDB = client.db('TechBuddyDB');
+    const productDB = techBuddyDB.collection('Product');
+
+    // insert into the database
+    app.use('/products', async (req, res) => {
+        const product = req.body;
+        const result = await productDB.insertOne(product);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
